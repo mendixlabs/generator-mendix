7fa6c4e944a46ef0b7e0f835450bed272f405dd0
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2015-10-28 14:57:26 +0100

    Finishing generator and adding a start-mendix command to Grunt

--- generators/app/templates/Gruntfile.js
+++ generators/app/templates/Gruntfile.js
@@ -1,14 +1,26 @@
 // Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %> :: http://github.com/<%= pkg.repository %>
 'use strict';
 
+// In case you seem to have trouble starting Mendix through `grunt start-mendix`, you might have to set the path to the Mendix application.
+// If it works, leave MODELER_PATH at null
+var MODELER_PATH = null;
+var MODELER_ARGS = '/file:{path}';
+
+// In case you have a different path to the test project (currently in ./test/Test.mpr) point TEST_PATH to the Test-project (full path). Otherwise, leave at null
+var TEST_PATH = null;
+
+var path = require('path'),
+    mendixApp = require('node-mendix-modeler-path'),
+    shelljs = require('shelljs');
+
 module.exports = function (grunt) {
   var pkg = grunt.file.readJSON("package.json");
   grunt.verbose;
   grunt.initConfig({
     watch: {
       autoDeployUpdate: {
-        "files": ["./src/**/*"],
-        "tasks": ["newer:copy", "compress"],
+        "files": [ "./src/**/*" ],
+        "tasks": [ "newer:copy", "compress" ],
         options: {
           debounceDelay: 250,
           livereload: true
@@ -33,14 +45,12 @@ module.exports = function (grunt) {
     copy: {
       deployment: {
         files: [
-          {dest: "./test/deployment/web/widgets", cwd: "./src/", src: ["**/*"], expand: true}//,
-          //{dest: "./test/Mx5.20/deployment/web/widgets", cwd: "./src/", src: ["**/*"], expand: true}
+          { dest: "./test/deployment/web/widgets", cwd: "./src/", src: ["**/*"], expand: true }
         ]
       },
       mpks: {
         files: [
-          {dest: "./test/widgets", cwd: "./dist/", src: [ pkg.name + ".mpk"], expand: true}//,
-          //{dest: "./test/Mx5.20/widgets", cwd: "./dist/", src: [ pkg.name + ".mpk"], expand: true}
+          { dest: "./test/widgets", cwd: "./dist/", src: [ pkg.name + ".mpk"], expand: true }
         ]
       }
     },
@@ -48,9 +58,7 @@ module.exports = function (grunt) {
       build: [
           "./dist/" + pkg.name + "/*",
           "./test/deployment/web/widgets/" + pkg.name + "/*",
-          "./test/widgets/" + pkg.name + ".mpk"/*,
-          "./test/Mx5.20/deployment/web/widgets/" + pkg.name + "/*",
-          "./test/Mx5.20/widgets/" + pkg.name + ".mpk"*/
+          "./test/widgets/" + pkg.name + ".mpk"
         ]            
     }
   });
@@ -61,11 +69,40 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-newer");
 
-  grunt.registerTask("default", ["watch"]);
+  grunt.registerTask("start-mendix", function () {
+    var done = this.async(),
+        testProjectPath = path.join(shelljs.pwd(), '/test/Test.mpr');
+
+    if (MODELER_PATH !== null || (mendixApp.err === null && mendixApp.output !== null && mendixApp.output.cmd && mendixApp.output.arg)) {
+      grunt.util.spawn({
+        cmd: MODELER_PATH || mendixApp.output.cmd,
+        args: [
+          (TEST_PATH !== null ? TEST_PATH : mendixApp.output.arg).replace('{path}', testProjectPath)
+        ]
+      }, function () {
+        done();
+      });
+    } else {
+      console.error('Cannot start Modeler, see error:');
+      console.log(mendixApp.err);
+      done();
+    }    
+  });
 
   grunt.registerTask(
-      "clean build",
-      "Compiles all the assets and copies the files to the build directory.", ["clean","compress", "copy" ]
-      );
-  grunt.registerTask("build", ["clean build"]);
+    "default",
+    "Watches for changes and automatically creates an MPK file, as well as copying the changes to your deployment folder",
+    [ "watch" ]
+  );
+
+  grunt.registerTask(
+    "clean build",
+    "Compiles all the assets and copies the files to the build directory.", 
+    [ "clean", "compress", "copy" ]
+  );
+
+  grunt.registerTask(
+    "build", 
+    [ "clean build" ]
+  );
 };
