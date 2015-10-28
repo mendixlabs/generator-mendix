1f45555b7d26c00bdd567e5256d621ffb6e1f11b
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2015-10-28 18:33:12 +0100

    Finishing up, adding tests

--- test/test-app.js
+++ test/test-app.js
@@ -3,22 +3,51 @@
 var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
-var os = require('os');
+var format = require('string-template');
 
-describe('mendix:app', function () {
+describe('Mendix generator:', function () {
+
+  var customWidgetName = 'TESTWIDGET';
+
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
+      .withPrompts({ widgetName: customWidgetName })
       .on('end', done);
   });
 
-  it('creates files', function () {
+  it('creates files from generator', function () {
     assert.file([
-      //'bower.json',
+      'Gruntfile.js',
       'package.json',
-      //'.editorconfig',
-      //'.jshintrc'
+      '.editorconfig',
+      '.gitignore'
     ]);
   });
+
+  it('copies generic files from AppStoreWidgetBoilerPlate', function () {
+    assert.file([
+      '.jshintrc',
+      'src/package.xml',
+      'test/Test.mpr',
+      'xsd/widget.xsd',
+      'assets/app_store_banner.png',
+      'assets/app_store_icon.png'
+    ]);
+  });
+
+  it(format('copies files from AppStoreWidgetBoilerPlate based on widgetName ({0})', customWidgetName), function () {
+    assert.file([
+      format('src/{0}/{0}.xml', [customWidgetName]),
+      format('src/{0}/lib/jquery-1.11.2.js', [customWidgetName]),
+      format('src/{0}/widget/{0}.js', [customWidgetName]),
+      format('src/{0}/widget/template/{0}.html', [customWidgetName]),
+      format('src/{0}/widget/ui/{0}.css', [customWidgetName])
+    ]);
+  });
+
+  // TODO: Check if the custom files use the correct prompts
+  // it('creates a custom package.json', function () {
+  //   assert.noFileContent('Gruntfile.js');
+  // });
 });
