3c396695a6ae0ecb3d83753ad06581e87d55c84d
Eric Tieniber <tieniber@gmail.com>
2019-08-26 12:40:22 -0400

    updated for gulp 4
    this package was failing with newer versions of node, due to a dependency of gulp-util which was removed as of gulp 4. This commit updates scaffolder to use gulp 4

--- generators/app/templates/Gulpfile.js
+++ generators/app/templates/Gulpfile.js
@@ -27,80 +27,89 @@ var pkg = require("./package.json"),
     paths = widgetBuilderHelper.generatePaths(pkg),
     xmlversion = widgetBuilderHelper.xmlversion;
 
-gulp.task("default", ['build'], function() {
-    gulp.watch("./src/**/*", ["compress"]);
-    gulp.watch("./src/**/*.js", ["copy:js"]);
-    gulp.watch("./src/**/*.html", ["copy:html"])
-});
-
-gulp.task("clean", function () {
-    return del([
-        paths.WIDGET_TEST_DEST,
-        paths.WIDGET_DIST_DEST
-    ], { force: true });
-});
-
-gulp.task("compress", ["clean"], function () {
-    return gulp.src("src/**/*")
-        .pipe(zip(pkg.name + ".mpk"))
-        .pipe(gulp.dest(paths.TEST_WIDGETS_FOLDER))
-        .pipe(gulp.dest("dist"));
-});
-
-gulp.task("copy:js", function () {
-    return gulp.src(["./src/**/*.js"])
-        .pipe(plumber(function (error) {
-            var msg = colors.red("Error");
-            if (error.fileName) {
-                msg += colors.red(" in ") + colors.cyan(error.fileName);
-            }
-            msg += " : " + colors.cyan(error.message);
-            log(msg);
-            this.emit("end");
-        }))
-        .pipe(jsValidate())
-        .pipe(newer(paths.TEST_WIDGETS_DEPLOYMENT_FOLDER))
-        .pipe(gulp.dest(paths.TEST_WIDGETS_DEPLOYMENT_FOLDER));
-});
-
-gulp.task("copy:html", function () {
-    return gulp.src(["./src/**/*.html"])
-        .pipe(newer(paths.TEST_WIDGETS_DEPLOYMENT_FOLDER))
-        .pipe(gulp.dest(paths.TEST_WIDGETS_DEPLOYMENT_FOLDER));
-});
-
-gulp.task("version:xml", function () {
-    return gulp.src(paths.PACKAGE_XML)
-        .pipe(xmlversion(argv.n))
-        .pipe(gulp.dest("./src/"));
-});
-
-gulp.task("version:json", function () {
-    return gulp.src("./package.json")
-        .pipe(gulpif(typeof argv.n !== "undefined", jsonTransform(function(data) {
-            data.version = argv.n;
-            return data;
-        }, 2)))
-        .pipe(gulp.dest("./"));
-});
-
-gulp.task("icon", function (cb) {
-    var icon = (typeof argv.file !== "undefined") ? argv.file : "./icon.png";
-    console.log("\nUsing this file to create a base64 string: " + colors.cyan(icon));
-    gulp.src(icon)
-        .pipe(intercept(function (file) {
-            console.log("\nCopy the following to your " + pkg.name + ".xml (after description):\n\n" + colors.cyan("<icon>") + file.contents.toString("base64") + colors.cyan("<\/icon>") + "\n");
-            cb();
-        }));
-});
-
-gulp.task("folders", function () {
-    paths.showPaths(); return;
-});
-
-gulp.task("modeler", function (cb) {
-    widgetBuilderHelper.runmodeler(MODELER_PATH, MODELER_ARGS, paths.TEST_PATH, cb);
-});
-
-gulp.task("build", ["compress"]);
-gulp.task("version", ["version:xml", "version:json"]);
+    gulp.task("clean", function () {
+        return del([
+            paths.WIDGET_TEST_DEST,
+            paths.WIDGET_DIST_DEST
+        ], { force: true });
+    });
+    
+    gulp.task("compress", gulp.series(["clean"], function () {
+        return gulp.src("src/**/*")
+            .pipe(zip(pkg.name + ".mpk"))
+            .pipe(gulp.dest(paths.TEST_WIDGETS_FOLDER))
+            .pipe(gulp.dest("dist"));
+    }));
+    
+    gulp.task("build", gulp.series(["compress"]));
+    
+    gulp.task("default", gulp.series(['build'], function() {
+        gulp.watch("./src/**/*", ["compress"]);
+        gulp.watch("./src/**/*.js", ["copy:js"]);
+        gulp.watch("./src/**/*.html", ["copy:html"])
+    }));
+    
+    gulp.task("compress", gulp.series(["clean"], function () {
+        return gulp.src("src/**/*")
+            .pipe(zip(pkg.name + ".mpk"))
+            .pipe(gulp.dest(paths.TEST_WIDGETS_FOLDER))
+            .pipe(gulp.dest("dist"));
+    }));
+    
+    gulp.task("copy:js", function () {
+        return gulp.src(["./src/**/*.js"])
+            .pipe(plumber(function (error) {
+                var msg = colors.red("Error");
+                if (error.fileName) {
+                    msg += colors.red(" in ") + colors.cyan(error.fileName);
+                }
+                msg += " : " + colors.cyan(error.message);
+                log(msg);
+                this.emit("end");
+            }))
+            .pipe(jsValidate())
+            .pipe(newer(paths.TEST_WIDGETS_DEPLOYMENT_FOLDER))
+            .pipe(gulp.dest(paths.TEST_WIDGETS_DEPLOYMENT_FOLDER));
+    });
+    
+    gulp.task("copy:html", function () {
+        return gulp.src(["./src/**/*.html"])
+            .pipe(newer(paths.TEST_WIDGETS_DEPLOYMENT_FOLDER))
+            .pipe(gulp.dest(paths.TEST_WIDGETS_DEPLOYMENT_FOLDER));
+    });
+    
+    gulp.task("version:xml", function () {
+        return gulp.src(paths.PACKAGE_XML)
+            .pipe(xmlversion(argv.n))
+            .pipe(gulp.dest("./src/"));
+    });
+    
+    gulp.task("version:json", function () {
+        return gulp.src("./package.json")
+            .pipe(gulpif(typeof argv.n !== "undefined", jsonTransform(function(data) {
+                data.version = argv.n;
+                return data;
+            }, 2)))
+            .pipe(gulp.dest("./"));
+    });
+    
+    gulp.task("icon", function (cb) {
+        var icon = (typeof argv.file !== "undefined") ? argv.file : "./icon.png";
+        console.log("\nUsing this file to create a base64 string: " + colors.cyan(icon));
+        gulp.src(icon)
+            .pipe(intercept(function (file) {
+                console.log("\nCopy the following to your " + pkg.name + ".xml (after description):\n\n" + colors.cyan("<icon>") + file.contents.toString("base64") + colors.cyan("<\/icon>") + "\n");
+                cb();
+            }));
+    });
+    
+    gulp.task("folders", function () {
+        paths.showPaths(); return;
+    });
+    
+    gulp.task("modeler", function (cb) {
+        widgetBuilderHelper.runmodeler(MODELER_PATH, MODELER_ARGS, paths.TEST_PATH, cb);
+    });
+    
+    gulp.task("version", gulp.parallel(["version:xml", "version:json"]));
+    
